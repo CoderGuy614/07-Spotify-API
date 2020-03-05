52f1dea38c1adcf88d603277039d2cc54ad6f638
CoderGuy614 <jlutz.110@gmail.com>
2020-03-05 10:13:21 +0800

    Added artists controller

--- controllers/artists.js
+++ controllers/artists.js
@@ -1,8 +1,19 @@
-const router = require('express').Router()
-const db = require('../db')
+const router = require("express").Router();
+const db = require("../db");
 
-router.get('/', (req, res) => {})
+router.get("/", (req, res) => {
+  db.query(`SELECT * FROM artists`, (err, results) => {
+    res.send(results);
+  });
+});
 
-router.get('/:id', (req, res) => {})
+router.get("/:id", (req, res) => {
+  db.query(
+    `SELECT * FROM artists where id = ${req.params.id}`,
+    (err, results) => {
+      res.send(results);
+    }
+  );
+});
 
-module.exports = router
+module.exports = router;
