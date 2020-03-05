2f20e34969c9c5f8bd31cb020b18df22ed9af48e
CoderGuy614 <jlutz.110@gmail.com>
2020-03-05 10:10:02 +0800

    Add genres controllers

--- controllers/genres.js
+++ controllers/genres.js
@@ -1,8 +1,19 @@
-const router = require('express').Router()
-const db = require('../db')
+const router = require("express").Router();
+const db = require("../db");
 
-router.get('/', (req, res) => {})
+router.get("/", (req, res) => {
+  db.query(`SELECT * FROM genres`, (err, results) => {
+    res.send(results);
+  });
+});
 
-router.get('/:id', (req, res) => {})
+router.get("/:id", (req, res) => {
+  db.query(
+    `SELECT * FROM genres where id = ${req.params.id}`,
+    (err, results) => {
+      res.send(results);
+    }
+  );
+});
 
-module.exports = router
+module.exports = router;
