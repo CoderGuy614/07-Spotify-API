ca0342fe603cf83870f743de3763692ad426faa3
CoderGuy614 <jlutz.110@gmail.com>
2020-03-05 16:06:41 +0800

    Added filters by req.body to songs controller

--- controllers/songs.js
+++ controllers/songs.js
@@ -2,22 +2,34 @@ const router = require("express").Router();
 const db = require("../db");
 
 router.get("/", (req, res) => {
-  db.query(
-    `SELECT songs.id as song_id, genres.name as genre, artists.name as artist_name, songs.name as song_name, albums.name as album_name, albums.cover as album_cover, songs.audio FROM songs
+  let key = Object.keys(req.query)[0];
+  let value = Object.values(req.query)[0];
+  let SQL = "";
+  if (!key) {
+    SQL = `SELECT songs.id as song_id, genres.name as genre, artists.name as artist_name, songs.name as song_name, albums.name as album_name, albums.cover as album_cover, songs.audio FROM songs
     LEFT JOIN albums 
     ON songs.album=albums.id
     LEFT JOIN artists 
     ON songs.artist=artists.id
     LEFT JOIN genres 
-    ON genres.id=songs.genre`,
-    (err, results) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send(results);
-      }
+    ON genres.id=songs.genre`;
+  } else {
+    SQL = `SELECT songs.id as song_id, genres.name as genre, artists.name as artist_name, songs.name as song_name, albums.name as album_name, albums.cover as album_cover, songs.audio FROM songs
+    LEFT JOIN albums 
+    ON songs.album=albums.id
+    LEFT JOIN artists 
+    ON songs.artist=artists.id
+    LEFT JOIN genres 
+    ON genres.id=songs.genre
+    WHERE songs.${key}=${value}`;
+  }
+  db.query(SQL, (err, results) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(results);
     }
-  );
+  });
 });
 
 module.exports = router;
