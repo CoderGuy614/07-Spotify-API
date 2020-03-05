16930d8421bdaf8bffe010431b2fe239bf1a1bb3
CoderGuy614 <jlutz.110@gmail.com>
2020-03-05 13:20:36 +0800

    Added Albums Controller

--- controllers/albums.js
+++ controllers/albums.js
@@ -1,8 +1,16 @@
-const router = require('express').Router()
-const db = require('../db')
+const router = require("express").Router();
+const db = require("../db");
 
-router.get('/', (req, res) => {})
+router.get("/", (req, res) => {
+  db.query(`SELECT * FROM albums`, (err, results) => {
+    res.send(results);
+  });
+});
 
-router.get('/:id', (req, res) => {})
+router.get("/:id", (req, res) => {
+  db.query(`SELECT * FROM albums WHERE id=${req.params.id}`, (err, results) => {
+    res.send(results);
+  });
+});
 
-module.exports = router
+module.exports = router;
