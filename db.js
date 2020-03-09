958f298c0be0f72a4745ebab48a66f117e310142
CoderGuy614 <jlutz.110@gmail.com>
2020-03-09 15:33:00 +0800

    Changed DB config

--- db.js
+++ db.js
@@ -1,18 +1,17 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "spotify",
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+  host: "us-cdbr-iron-east-04.cleardb.net",
+  user: "b1ee4b433ac203",
+  password: "25b01381",
+  database: "heroku_497550bddc8d143"
 });
 
 db.connect(err => {
   if (err) {
     console.log({ err });
   } else {
-    console.log("Connected to MySQL");
+    console.log("Connected");
   }
 });
 
