b70c8ed90b03b6c7b9ff90a5adf6a40f080be944
CoderGuy614 <jlutz.110@gmail.com>
2020-03-09 15:57:42 +0800

    removed socketpath

--- db.js
+++ db.js
@@ -4,8 +4,7 @@ const db = mysql.createConnection({
   host: "us-cdbr-iron-east-04.cleardb.net",
   user: "b1ee4b433ac203",
   password: "25b01381",
-  database: "heroku_497550bddc8d143",
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+  database: "heroku_497550bddc8d143"
 });
 
 db.connect(err => {
