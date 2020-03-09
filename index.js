c0f0d4bea2b00221e5e0d6408551ede32053ff15
CoderGuy614 <jlutz.110@gmail.com>
2020-03-09 16:19:27 +0800

    added cors credentials

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 // Require
-const express = require('express')
-const bodyParser = require('body-parser')
-require('dotenv').config()
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const cors = require("cors");
 
 // App
-const app = express()
+const app = express();
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors({ credentials: true }));
 
 // Routes
-app.use('/artists', require('./controllers/artists'))
-app.use('/songs', require('./controllers/songs'))
-app.use('/albums', require('./controllers/albums'))
-app.use('/genres', require('./controllers/genres'))
-app.use('/pay', require('./controllers/pay'))
+app.use("/artists", require("./controllers/artists"));
+app.use("/songs", require("./controllers/songs"));
+app.use("/albums", require("./controllers/albums"));
+app.use("/genres", require("./controllers/genres"));
+app.use("/pay", require("./controllers/pay"));
 
 // Server
 app.listen(process.env.PORT, err => {
-	err
-		? () => {
-				throw err
-		  }
-		: console.log(`Ready on port ${process.env.PORT}`)
-})
-module.exports = app
+  err
+    ? () => {
+        throw err;
+      }
+    : console.log(`Ready on port ${process.env.PORT}`);
+});
+module.exports = app;
